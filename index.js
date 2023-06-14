7b370d6cfe27d01f9536d6bd8a4554c57efbd6fa
sdjlskdj <136450043+sdjlskdj@users.noreply.github.com>
2023-06-14 18:21:29 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||罗婷|祝你|生日|快乐|A``I`I``A|');
+      S.UI.simulate('|#countdown 3||罗婷|祝你|生日|快乐|[``i`i``]|');
     }
 
     S.Drawing.loop(function () {
